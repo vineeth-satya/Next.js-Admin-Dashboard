734677a35094e2d7c68f4807af485aaa8e1cd687
Satya  Vineeth <110622240+satya-vineeth@users.noreply.github.com>
2024-07-05 13:15:58 +0530

    pushed

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+const Homepage = () => {
+  return (
+    <div>Please Change the route to localhost:3000/dashboard</div>
+  )
+}
+
+export default Homepage
---
+++ app/ui/dashboard/card/card.jsx
@@ -0,0 +1,23 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./card.module.css";
+
+const Card = ({ item }) => {
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}></span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>
+            12%
+          </span>
+          more than previous week
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
